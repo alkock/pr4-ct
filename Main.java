663151b673cb81e7e5cb359e75caaf951380925e
Leon Schardin <leon.schardin@gmail.com>
2023-05-16 13:48:56 +0200

    verbessert2

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World! Hallo tester");
+        System.out.println("Hello World! Hallobb tester");
     }
 }
