79db635bf92a6a69a36c8daa34e452d5e53e47b4
Marvin <test@web.de>
2023-05-22 13:03:00 +0200

    TEST-M

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World! Hallobb tester");
+        System.out.println("Hello World! Hallobb tester XXX");
     }
     //Endlich
 }
